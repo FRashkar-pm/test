c96eb8c9a685c1c2bada477dc12f0a7458078d43
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-09-27 19:12:04 +0700

    Create test.php

---
+++ src/CHL/test/test.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace\CHL\test;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\EventListener;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\item\Item;
+use pocketmine\effect\Effect;
+use pocketmine\entity\Entity;
+use pocketmine\entity\Living;
+use pocketmine\event\entity\EntityRegainHealthEvent;
+
+class test extends PluginBase implements Listener;{
+        public function onLoad (){
+                $this->getLogger()->info("Memuat plugin untuk uji coba!");
+        }
+        public function onEnable(){
+                $this->getServer()->getPluginManager()->registerEvents($this,$this);
+                $this->getLogger()->info("Plugin ini dibuat untuk uji coba!");
+        }
+        public function onDisable(){
+                $this->getLogger()->info("Plugin tidak aktif!");
+        }
+        public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+                if($cmd->getName() == "coba"){
+                    if(!$sender instanceof Player){
+                        $sender->sendMessage("Tolong gunakan perintah ini di dalam game!");
+                    }else{
+                        $sender->getInventory()->addItem(Item::get(364,0,8));
+                        $sender->sendMessage("Kamu mendapatkan 8 steak!");
+                    }
+                    return true;
+                }
+        }
+}
